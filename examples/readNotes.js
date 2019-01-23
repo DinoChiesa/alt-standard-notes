545c2a55970b99592e187a23de6c109c6f67dd24
Dino Chiesa <dpchiesa@hotmail.com>
2019-01-23 14:24:07 -0800

    update readNotes example with more features
      - now the --quiet option really works
      - there is a --save option that writes the full list of notes to fs
      - the --showOne option is explicit

--- examples/readNotes.js
+++ examples/readNotes.js
@@ -1,7 +1,7 @@
 // readNotes.js
 // ------------------------------------------------------------------
 //
-// read notes stored in standard notes for a particular user.
+// read notes stored in standard notes for a particular user, and optionally save a decrypted copy of them.
 //
 
 /* jshint esversion: 6, node: true, strict: false, camelcase : false */
@@ -12,9 +12,10 @@ const sn           = require('alt-standard-notes'),
       sprintf      = require('sprintf-js').sprintf,
       readlineSync = require('readline-sync'),
       url          = require('url'),
-      version      = '20181126-1115';
+      path         = require('path'),
+      version      = '20190123-1047';
 
-var options = {tryUseNetRc : true, quiet : false, email : null};
+var options = {tryUseNetRc:true, quiet:false, email:null, save:false, showOne:false};
 
 function getPassword() {
   let baseUrl = sn.getDefaultBaseUrl();
@@ -28,36 +29,85 @@ function getPassword() {
 }
 
 function usage() {
-  var path = require('path');
-  console.log('usage:\n  %s --email xxx@example.org [--nonetrc] [--quiet]', path.basename(process.argv[1]));
+  console.log('usage:\n  %s --email xxx@example.org [--nonetrc] [--quiet] [--save]', path.basename(process.argv[1]));
   process.exit(1);
 }
 
+function getClientUpdatedAt(item) {
+  return item.content.appData['org.standardnotes.sn'].client_updated_at;
+}
+
+function byClientUpdatedAt(item1, item2) {
+  var a = getClientUpdatedAt(item1),
+      b = getClientUpdatedAt(item2);
+  if (a < b) {
+    return -1;
+  }
+  return (a > b) ? 1: 0;
+}
+
+function maybeSerializeAllRecords(allNotesAndTags) {
+  if (options.save) {
+  const dateformat   = require('dateformat'),
+        fs           = require('fs'),
+        now          = dateformat(new Date(), 'yyyymmmdd-HHMMss') + 'UTC',
+        dataFilename = path.join('./',
+                                 sprintf('standardnotes-snapshot-%s-%s.json', options.email, now));
+  fs.writeFileSync(dataFilename, JSON.stringify(allNotesAndTags));
+    if ( ! options.quiet) {
+      console.log('\nSaved file %s', dataFilename);
+    }
+  }
+}
+
+function maybeShowAllRecords(allNotesAndTags) {
+  if ( options.quiet) {
+    return allNotesAndTags;
+  }
+  let allnotes = allNotesAndTags.filter((item) => (item.content_type === 'Note' && !item.deleted && item.content) );
+  console.log('\nretrieved %d items', Object.keys(allnotes).length);
+  let maxTitleLength = 0;
+  allnotes
+    .forEach( (item) => {
+      if (item.content.title.length > maxTitleLength) {
+        maxTitleLength = item.content.title.length;
+      }
+    });
+  let formatStr = sprintf('%%3d. %%-%ds  %%-24s   %%-24s', maxTitleLength);
+  allnotes
+    .sort( byClientUpdatedAt )
+    .forEach( (item, ix) => {
+      console.log(sprintf(formatStr, ix + 1,
+                          item.content.title,
+                          item.content.appData['org.standardnotes.sn'].client_updated_at,
+                          item.updated_at ));
+    });
+  return allNotesAndTags;
+}
+
 function readAllRecords(connection) {
-  var aggregated = {};
-  var showOne = false;
+  var aggregated = [];
+  var showOne = options.showOne;
   const BATCH_SIZE = 125;
   let readBatch = function(cursor_token) {
         return new Promise( (resolve) => {
           sn.readNotes(connection, { limit: BATCH_SIZE, cursor_token: cursor_token} )
             .then ( (result) => {
-              process.stdout.write('.');
+              if ( ! options.quiet) {
+                process.stdout.write('.');
+              }
               result.retrieved_items.forEach( (item) => {
                 if (item.content_type === 'Note' && !item.deleted && item.content) {
                   if (showOne) {
                     console.log('read item: ' + JSON.stringify(item));
                     showOne = false;
                   }
-                  var content = JSON.parse(item.content);
-                  aggregated[item.uuid] = {
-                    title:content.title || '??',
-                    updated_at: item.updated_at,
-                    client_updated_at: content.appData['org.standardnotes.sn'].client_updated_at
-                  };
+                  item.content = JSON.parse(item.content);
                 }
               });
+              aggregated = aggregated.concat(result.retrieved_items);
               if (result.cursor_token) {
-                return resolve(readBatch(result.cursor_token) );
+                return resolve( readBatch(result.cursor_token) );
               }
               return resolve(aggregated);
             });
@@ -66,18 +116,6 @@ function readAllRecords(connection) {
   return readBatch(null);
 }
 
-function byNamedProperty(property, all) {
-  return (key1, key2) => {
-    let a = all[key1], b = all[key2];
-    if (a[property] < b[property]) {
-      return -1;
-    }
-    if (a[property] > b[property]) {
-      return 1;
-    }
-    return 0;
-  };
-}
 
 function main(args) {
   var awaiting = null;
@@ -90,6 +128,14 @@ function main(args) {
 
     case '--nonetrc':
       options.tryUseNetRc = false;
+      break;
+
+    case '--save':
+      options.save = true;
+      break;
+
+    case '--showOne':
+      options.showOne = true;
       break;
 
     case '--quiet':
@@ -118,28 +164,17 @@ function main(args) {
         'Node.js ' + process.version + '\n');
   }
 
+  if ( ! options.email) {
+    console.log('you must specify --email');
+    usage();
+  }
+
   sn.signin({email:options.email, getPasswordFn: getPassword})
     .then( readAllRecords )
-    .then( (all) => {
-      console.log('\nretrieved %d items', Object.keys(all).length);
-      let maxTitleLength = 0;
-      Object.keys(all)
-        .forEach( (key) => {
-          var item = all[key];
-          if (item.title.length > maxTitleLength) {
-            maxTitleLength = item.title.length;
-          }
-        });
-      let formatStr = sprintf('%%3d. %%-%ds  %%-24s   %%-24s', maxTitleLength);
-      Object.keys(all)
-        .sort( byNamedProperty('client_updated_at', all) )
-        .forEach( (key, ix) => {
-          var item = all[key];
-          console.log(sprintf(formatStr, ix + 1, item.title, item.client_updated_at, item.updated_at ));
-        });
-    })
+    .then( maybeShowAllRecords )
+    .then( maybeSerializeAllRecords )
     .catch( (error) => {
-      console.log('error: ' + JSON.stringify(error));
+      console.log('error: ' + error.stack);
     });
 }
 
